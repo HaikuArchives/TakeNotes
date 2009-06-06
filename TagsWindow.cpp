129f36b4100577287a6624cfe176d8af6ccc24dd
boser <boser@0c836dc9-e4c2-4435-8f31-f236467544ac>
2009-06-06 11:50:01 +0000

    ripulito il codice di tags window, eliminate variabili superflue
    git-svn-id: http://svn.osdrawer.net/takenotes@55 0c836dc9-e4c2-4435-8f31-f236467544ac

--- TagsWindow.cpp
+++ TagsWindow.cpp
@@ -7,7 +7,7 @@
  *			Ilio Catallo, 
  *			Stefano Celentano
  * 
- * Last revision: Ilio Catallo, 6th June 2009
+ * Last revision: Stefano Celentano, 7th June 2009
  *
  * Description: TODO
  */
@@ -43,31 +43,26 @@ TagsWindow :: TagsWindow(BMessage *fSaveMessage)
 		char			buffer_tag2[30];
 		char			buffer_tag3[30];
 									
-		//Create the main view
-		
+		//Create the main view	
 		fTagsView = new BView(Bounds(), "TagsView", B_FOLLOW_ALL, B_WILL_DRAW | B_FRAME_EVENTS);
 		 
 		
 		//Create the Text Field 
-		
 		fTag1 = new BTextControl(BRect(20,40,200,55), "tag1", "First Tag: ", NULL, NULL);
 		fTag2 = new BTextControl(BRect(20,70,200,85), "tag2", "Second Tag: ", NULL, NULL);
 		fTag3 = new BTextControl(BRect(20,100,200,135), "tag3", "Third Tag: ", NULL, NULL);
 
-		//Create the OK and UNDO button
-		
+		//Create the OK and UNDO button	
 		fDoneButton = new BButton(BRect(340,200,390,210), "ok", "OK", new BMessage(BUTTON_OK));
 		fUndoButton = new BButton(BRect(270,200,320,210), "undo", "Undo", new BMessage(BUTTON_UNDO));
 		
 		//Add the tag TextControl as children
-		
 		AddChild(fTagsView);
 		fTagsView -> AddChild(fTag1);
 		fTagsView -> AddChild(fTag2);
 		fTagsView -> AddChild(fTag3);
 		
 		//Add OK and UNDO button as children
-		
 		fTagsView -> AddChild(fDoneButton);
 		fTagsView -> AddChild(fUndoButton);
 		
@@ -89,9 +84,6 @@ TagsWindow :: TagsWindow(BMessage *fSaveMessage)
 		fFile.ReadAttr("TAKENOTES:tagone", B_STRING_TYPE, 0, &buffer_tag1, 30);
 		fFile.ReadAttr("TAKENOTES:tagtwo", B_STRING_TYPE, 0, &buffer_tag2, 30);
 		fFile.ReadAttr("TAKENOTES:tagthree", B_STRING_TYPE, 0, &buffer_tag3, 30);
-
-		printf(buffer_tag1);
-		printf("\n");
 			
 		//Initialize text field to old values
 		fTag1 -> SetText(buffer_tag1);
@@ -105,73 +97,27 @@ TagsWindow :: ~TagsWindow(){
 	 if (fFile.InitCheck() == B_OK)
 			fFile.Unset();
 		
-
 }
 
 void TagsWindow :: MessageReceived(BMessage *message){
 
 			switch(message->what){
 				case BUTTON_OK: {
-				
-						//Variables
-						
-				
-						//Obtain the BNode object (I need to obtain the generic entry_ref, I still don't know how!)
-				
-						/*BNode node = BNode(&ref_tags);
-						if(node.InitCheck() != B_OK) {
-							printf("The object wasn't correctly inizialized\n");
-						}
-						*/
-						
-		
-						//Print all the attribute associated with the BNode object (only for debugging)
-					
-						
-						char buffer[60];
-		
-						while(fFile.GetNextAttrName(buffer) == B_OK) {
-							printf(">Attr name: %s \n", buffer);
-						}
-									
-																	
-						//Set each attribute to text field's content (NOTICE: add some check, like empty check field)
-												
-						
-						if(fFile.WriteAttr("TAKENOTES:tagone", B_STRING_TYPE, 0, fTag1 -> Text(), 20) == 0){
+																			
+						//Set each attribute to text field's content
+						if(fFile.WriteAttr("TAKENOTES:tagone", B_STRING_TYPE, 0, fTag1 -> Text(), 30) == 0){
 							printf("no bytes written for tag one\n");
 						}
 								
-						if(fFile.WriteAttr("TAKENOTES:tagtwo", B_STRING_TYPE, 0, fTag2 -> Text(), 20) == 0){
+						if(fFile.WriteAttr("TAKENOTES:tagtwo", B_STRING_TYPE, 0, fTag2 -> Text(), 30) == 0){
 							printf("no bytes written for tag two\n");
 						}
 								
-						if(fFile.WriteAttr("TAKENOTES:tagthree", B_STRING_TYPE, 0, fTag3 -> Text(), 20) == 0){
+						if(fFile.WriteAttr("TAKENOTES:tagthree", B_STRING_TYPE, 0, fTag3 -> Text(), 30) == 0){
 							printf("no bytes written for tag three\n");
 						}													
-						
-						/*
-						//Print ALL the attributes (only for debugging)
-						
-						char buffer2[60];
-						
-						node.ReadAttr("TAKENOTES:tagone", B_STRING_TYPE, 0, &buffer2, 30);
-						printf("First tag is: ");
-						printf(buffer2);
-						printf("\n");
-						
-						node.ReadAttr("TAKENOTES:tagtwo", B_STRING_TYPE, 0, &buffer2, 30);
-						printf("Second tag is: ");
-						printf(buffer2);
-						printf("\n");
-						
-						node.ReadAttr("TAKENOTES:tagthree", B_STRING_TYPE, 0, &buffer2, 30);
-						printf("Third tag is: ");
-						printf(buffer2);
-						printf("\n");*/
-			
+									
 						//Closing window
-						
 						Quit();		
 																											
 				}
